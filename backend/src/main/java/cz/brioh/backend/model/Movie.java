7f1488b31839a90baa915cea6594272eaec50f2c
DavSonCZE <40635601+DavSonCZE@users.noreply.github.com>
2023-04-02 18:40:22 +0200

    added movie table | 02/04/23

---
+++ backend/src/main/java/cz/brioh/backend/model/Movie.java
@@ -0,0 +1,70 @@
+package cz.brioh.backend.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "movies")
+
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotNull
+    private String title;
+
+    private long year;
+
+    private String thumbnail;
+
+    private String description;
+
+    public Movie(long id, String title, long year, String thumbnail, String description) {
+        this.id = id;
+        this.title = title;
+        this.year = year;
+        this.thumbnail = thumbnail;
+        this.description = description;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public long getYear() {
+        return year;
+    }
+
+    public void setYear(long year) {
+        this.year = year;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(String thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
